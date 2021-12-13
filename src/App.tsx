6d4074567a62d6c1575ce1b75bd647f2de0f05da
ksugimori <ksugimori.private@gmail.com>
2021-12-13 21:30:50 +0900

    複数行に対応

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import Cell from './components/Cell';
 import { useGame } from './state/GameProvider';
 
 function App() {
-  const { game } = useGame();
+  const { field } = useGame();
+
+  const divs = field.rows
+    // 各要素を <Cell> に変換
+    .map((row, y) => {
+      return row.map((cell, x) => {
+        return <Cell key={`${x}:${y}`} {...cell} />;
+      });
+    })
+    // 行単位で <div> で囲う
+    .map((cells, y) => <div key={y} className='flex-row'>{cells}</div>);
 
   return (
     <div className="App">
-      {game.cells[0].map(cell => <Cell {...cell} />)}
+      {divs}
     </div>
   );
 }
--- src/state/GameProvider.tsx
+++ src/state/GameProvider.tsx
@@ -2,34 +2,43 @@ import React, { createContext, useState, useContext } from 'react';
 import CellState from '../models/CellState';
 
 // TODO: クラス化？
-interface Game {
-  cells: CellState[][];
+interface Field {
+  rows: CellState[][];
 }
-interface IGameContext {
-  game: Game;
+interface Game {
+  field: Field;
   openCell: (x: number, y: number) => void
 }
 
-
-const defaultGame: Game = {
-  cells: [
+const field: Field = {
+  rows: [
     [
       { count: 1, isOpen: false },
       { count: 2, isOpen: true },
       { count: 3, isOpen: true },
+    ],
+    [
+      { count: 4, isOpen: true },
+      { count: 5, isOpen: false },
+      { count: 6, isOpen: true },
+    ],
+    [
+      { count: 7, isOpen: true },
+      { count: 8, isOpen: true },
+      { count: 9, isOpen: false },
     ]
   ]
 };
-const GameContext: React.Context<IGameContext> = createContext({
-  game: defaultGame, openCell: (x, y) => {
-
-  }
-});
+const GameContext: React.Context<Game> = createContext({ field, openCell });
 export const useGame = () => useContext(GameContext);
+
+function openCell(x: number, y: number) {
+  console.log(`click (${x}, ${y})`)
+}
 
 export default function GameProvider(props: any) {
   return (
-    <GameContext.Provider value={{ game: defaultGame, openCell: (x, y) => console.log(`click (${x}, ${y})`) }}>
+    <GameContext.Provider value={{ field, openCell }}>
       {props.children}
     </GameContext.Provider>
   );
