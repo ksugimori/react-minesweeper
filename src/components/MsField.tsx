aa5214e4d6daa16c770db12c579e636e239f4420
ksugimori <ksugimori.private@gmail.com>
2021-12-21 22:30:25 +0900

    MsFieldRow を削除

--- src/components/MsField.tsx
+++ src/components/MsField.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './MsField.scss';
 import { useGame } from '../state/GameProvider'
-import MsFieldRow from './MsFieldRow';
+import MsCell from './MsCell';
+import Cell from '../models/Cell';
 
+/**
+ * MsField の１行を作る
+ * @param cells この行に表示するセル
+ * @param y Y座標
+ * @returns 行
+ */
+const toFieldRow = (cells: Cell[], y: number) => (
+  <div className='ms-field-row' key={y}>
+    {cells.map(cell => <MsCell key={cell.id} {...cell} />)}
+  </div>
+);
+
+/**
+ * フィールド
+ * @returns MsField
+ */
 function MsField() {
   const { field } = useGame();
 
   return (
-    <div className="field">
-      {field.rows.map((row, y) => <MsFieldRow key={y} cells={row}></MsFieldRow>)}
+    <div className="ms-field">
+      {field.rows.map(toFieldRow)}
     </div>
   );
 }
